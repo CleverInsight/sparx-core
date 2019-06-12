5b3cadfc87fa5a5773d1e3024e915c2358e651a5
Bastin Robins J <bastinrobins@gmail.com>
2019-06-12 10:25:47 +0530

    Modified: Enabled convert_req_to_csv function

--- sparx/core/skeleton/model_controller.py
+++ sparx/core/skeleton/model_controller.py
@@ -10,18 +10,33 @@ from sklearn.externals import joblib
 
 
 def convert_req_to_csv(fileinfo):
-    ''' Get the batch file from request payload and convert to pandas 
-    dataframes
+    ''' Get the batch file from request payload and convert to pandas dataframes
+
+    Parameters:
+    -----------
+        fileinfo: Dict
+            Enter a dictionary from payload which consist of filename, content
+            body
+    
+    Usage:
+    ------
+        >>> convert_req_to_csv({filename: xyz, content: 'dsadsad'})
+        >>> True
     '''
-    fname = fileinfo['filename']
-
     # Actual filename
-    extn = os.path.splitext(fname)[1]
+    extn = os.path.splitext(fileinfo['filename'])[1]
 
-    # # New filename
-    cname = str(uuid.uuid4()) + extn
-    fh = open('temp.csv', 'w') 
-    fh.write(fileinfo['body'])
+    random_filename = str(uuid.uuid4()) + extn
+
+    # Generate file 
+    temp_path = os.path.join(os.path.abspath(\
+        os.path.join(os.path.dirname(os.path.abspath(__file__)),"..", 'resources')), 'temp', random_filename)
+    
+    # Save batch test file
+    with open(temp_path, 'w') as f:
+        f.write(fileinfo['body'])
+    
+    return temp_path if os.path.exists(temp_path) else None
 
 
 
@@ -46,23 +61,11 @@ class controller_name(base_handler):
         clf = joblib.load(os.path.join(os.path.abspath(\
             os.path.join(os.path.dirname(os.path.abspath(__file__)),"..", 'resources')), 'models', 'controller_name.pkl'))
 
-        fileinfo = self.request.files['batch'][0]
-
-        # Actual filename
-        extn = os.path.splitext(fileinfo['filename'])[1]
-
-        random_filename = str(uuid.uuid4()) + extn
-
-        # Generate file 
-        temp_path = os.path.join(os.path.abspath(\
-            os.path.join(os.path.dirname(os.path.abspath(__file__)),"..", 'resources')), 'temp', random_filename)
-        
-        # Save batch test file
-        with open(temp_path, 'w') as f:
-            f.write(fileinfo['body'])
+        # Save batch file into directory
+        batch_path = convert_req_to_csv(self.request.files['batch'][0])
 
         # Read saved file using pandas dataframe
-        x_test = pd.read_csv(temp_path)
+        x_test = pd.read_csv(batch_path)
 
         # Batch prediction    
         predicted = clf.predict(x_test.values)
@@ -76,4 +79,3 @@ class controller_name(base_handler):
 
         self.write(x_test.reset_index().to_json(orient='records'))
         self.finish()
-
